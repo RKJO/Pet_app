f8ca1b04b4075ce937fd137ab5cee1d62b83d652
RKJO <rkjozwiak@gmail.com>
2018-09-04 09:06:10 +0200

    first site by site crawl spider

--- tutorial/spiders/shelter_paluch_spider.py
+++ tutorial/spiders/shelter_paluch_spider.py
@@ -2,26 +2,31 @@ import scrapy
 
 
 class AnimalSpider(scrapy.Spider):
-    name = "Animal"
-    start_urls = [
-        'http://www.napaluchu.waw.pl/czekam_na_ciebie/wszystkie_zwierzeta_do_adopcji/011701269'
-    ]
+    name = "animals"
+
+    def start_requests(self):
+        urls = [
+            'http://www.napaluchu.waw.pl/czekam_na_ciebie/wszystkie_zwierzeta_do_adopcji:1'
+        ]
+        # for url in urls:
+        yield scrapy.Request(url=urls[0], callback=self.next_page)
+
+    def next_page(self, response):
+        pages = response.css('.animals_btn_list_more::attr(href)').extract()
+        for page in pages:
+            if page is not None:
+                page = response.urljoin(page)
+                yield scrapy.Request(page, callback=self.parse)
 
     def parse(self, response):
-
-        # for info in response.css('.info'):
-
-            yield {
-                'name': response.css('.info').css('h5::text').extract_first().split(' ')[-5],
-                'species': response.css('.info').css('span::text').extract()[0].split(':')[-1].strip(),
-                'race': response.css('.info').css('span::text').extract()[1].split(':')[-1].strip(),
-                'sex': response.css('.info').css('span::text').extract()[2].split(':')[-1].strip(),
-                'age': response.css('.info').css('span::text').extract()[3].split(':')[-1].strip(),
-                'weight': response.css('.info').css('span::text').extract()[4].split(' ')[1],
-                'admission_date': response.css('.info').css('span::text').extract()[5].split(' ')[-1],
-                'evidence_number': response.css('.info').css('span::text').extract()[6].split(' ')[-1],
-                'description': ' '.join(' '.join(response.css('.description::text').extract()).split()),
-            }
-
-
-    # response.css('.animals_btn_list_more::attr(href').extract()
+        yield {
+            'name': response.css('.info').css('h5::text').extract_first().split(' ')[-5],
+            'species': response.css('.info').css('span::text').extract()[0].split(':')[-1].strip(),
+            'race': response.css('.info').css('span::text').extract()[1].split(':')[-1].strip(),
+            'sex': response.css('.info').css('span::text').extract()[2].split(':')[-1].strip(),
+            'age': response.css('.info').css('span::text').extract()[3].split(':')[-1].strip(),
+            'weight': response.css('.info').css('span::text').extract()[4].split(' ')[1],
+            'admission_date': response.css('.info').css('span::text').extract()[5].split(' ')[-1],
+            'evidence_number': response.css('.info').css('span::text').extract()[6].split(' ')[-1],
+            'description': ' '.join(' '.join(response.css('.description::text').extract()).split()),
+        }
