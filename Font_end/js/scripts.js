705d89100efaec488ec4f7217214d8e9805132b0
RKJO <rkjozwiak@gmail.com>
2018-09-21 19:53:23 +0200

    add-ed pagination

--- Font_end/js/scripts.js
+++ Font_end/js/scripts.js
@@ -39,6 +39,7 @@ function initMapDetails() {
 
 $(function() {
   var $animals_cards = $("#animal-cards");
+  var $listpagination = "#animal-list-pagination";
   $.ajax({
     url: "http://127.0.0.1:8000/animals/",
     method: "GET"
@@ -79,5 +80,37 @@ $(function() {
       $button.insertAfter($weight);
       $card_body.insertAfter($img);
     }
+
+    var $page_item = $();
+    var $previous = $(
+      '<li class="page-item"><a class="page-link" href="#"><span>&laquo;</span><span class="sr-only">Previous</span></a>'
+    );
+    $previous.attr("href", response.previous);
+    var $next = $(
+      '<li class="page-item"><a class="page-link" href="#"><span>&raquo;</span><span class="sr-only">Next</span></a></li>'
+    );
+    $next.attr("href", response.next);
+
+    // <li class="page-item disabled">
+    //         <a class="page-link" href="#">
+    //             <span>&laquo;</span>
+    //             <span class="sr-only">Previous</span>
+    //         </a>
+    //     </li>
+    //     <li class="page-item active">
+    //         <a class="page-link" href="#">1</a>
+    //     </li>
+    //     <li class="page-item">
+    //         <a class="page-link" href="#">2</a>
+    //     </li>
+    //     <li class="page-item">
+    //         <a class="page-link" href="#">3</a>
+    //     </li>
+    // <li class="page-item">
+    //     <a class="page-link" href="#">
+    //         <span>&raquo;</span>
+    //         <span class="sr-only">Next</span>
+    //     </a>
+    // </li>
   });
 });
