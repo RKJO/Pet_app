d936d361576c94d41d8ea02ec4c15d5ed1b83e46
RKJO <rkjozwiak@gmail.com>
2018-09-21 19:12:45 +0200

    fix button jQuery

--- Font_end/js/scripts.js
+++ Font_end/js/scripts.js
@@ -34,41 +34,6 @@ function initMapDetails() {
     options
   );
 }
-<<<<<<< Updated upstream
-// $(function() {
-
-//     var $animals_cards = $('#animal-cards');
-//     $.ajax({
-//         url: 'http://127.0.0.1:8000/animals/',
-//         method: 'GET'
-//     }).done(function(response) {
-
-//         for(var i= 0; i < response.length; i++) {
-//             var $li = $('<li data-id="' + response[i].id + '">');
-//             $li.text(response[i].title);
-//             var $div = $('<div class="description hidden">');
-//             var $del = $('<a href="#" class="del" style="margin-left: 10px">');
-//             $del.text('Usuń');
-//             $gitanimals_cards.append($li);
-//             $li.append($del);
-//             $div.insertAfter($li);
-
-//             $del.on('click', function(event) {
-//                 var $id = $(this).parent().data('id');
-//                 $.ajax({
-//                     url: 'http://127.0.0.1:8000/book/' + $id,
-//                     method: 'DELETE',
-//                     data: 'pk="' + $id +'"'
-//                 }).done(function() {
-//                     location.reload();
-//                     alert('Książka usunięta');
-//                 });
-
-//             });
-//         }
-
-//     });
-=======
 
 // Get animal list
 
@@ -78,42 +43,41 @@ $(function() {
     url: "http://127.0.0.1:8000/animals/",
     method: "GET"
   }).done(function(response) {
-    for (var i = 0; i < response.length; i++) {
+    for (var i = 0; i < 15; i++) {
       var $card = $('<div class="card">');
       var $img = $(
         '<img src="' +
           response[i].img_main +
           '" alt="' +
           response[i].img_main_alt +
-          '" class="img-fluid card-im g-top">'
+          '" class="img-fluid card-img-top">'
       );
       var $card_body = $('<div class="card-body">');
       var $title = $('<h4 class="card-title"></h4>');
       $title.text(response[i].name);
-      var $race = $('<small class="text-muted"></small><hr>');
+      var $race = $('<small class="text-muted"></small>');
       $race.text(response[i].race);
-      var $sex = $('<p class="card-text"</p>');
-      $sex.text(response[i].sex);
-      var $age = $('<p class="card-text"</p>');
-      $age.text(response[i].age);
-      var $weight = $('<p class="card-text"</p>');
-      $weight.text(response[i].weight);
-      var $button = $(
-        '<a class="btn btn-outline-primary btn-block" href="/animals/' +
-          response[i].id +
-          ">Więcej szczegółów</a>"
-      );
+      var $hr = $("<hr>");
+      var $sex = $('<p class="card-text"></p>');
+      $sex.text("Płeć: " + response[i].sex);
+      var $age = $('<p class="card-text"></p>');
+      $age.text("wiek: " + response[i].age);
+      var $weight = $('<p class="card-text"></p>');
+      $weight.text("Wielkość: " + response[i].weight);
+      var $button = $('<a class="btn btn-outline-primary btn-block" href=#>');
+      $button.attr("href", "/animals/" + response[i].id);
+      $button.text("Więcej szczegółów");
 
       $animals_cards.append($card);
       $card.append($img);
       $card_body.append($title);
       $race.insertAfter($title);
-      $sex.insertAfter($race);
+      $hr.insertAfter($race);
+      $sex.insertAfter($hr);
       $age.insertAfter($sex);
       $weight.insertAfter($age);
       $button.insertAfter($weight);
-      $card_body.isertAfter($img);
+      $card_body.insertAfter($img);
     }
   });
 });
->>>>>>> Stashed changes
