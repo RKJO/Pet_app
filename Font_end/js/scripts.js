4fd9cdd7e708a3971c7a7cd389cc61cee7f3e349
RKJO <rkjozwiak@gmail.com>
2018-09-23 13:13:48 +0200

    some edits with pagination

--- Font_end/js/scripts.js
+++ Font_end/js/scripts.js
@@ -39,34 +39,34 @@ function initMapDetails() {
 
 $(function() {
   var $animals_cards = $("#animal-cards");
-  var $listpagination = "#animal-list-pagination";
-  $.ajax({
-    url: "http://127.0.0.1:8000/animals/",
-    method: "GET"
-  }).done(function(response) {
-    for (var i = 0; i < 15; i++) {
+  var $listpagination = $("#animal-list-pagination");
+
+  function getAiimalList(response) {
+    var $result = response["results"];
+
+    for (var i = 0; i < $result.length; i++) {
       var $card = $('<div class="card">');
       var $img = $(
         '<img src="' +
-          response[i].img_main +
+          $result[i].img_main +
           '" alt="' +
-          response[i].img_main_alt +
+          $result[i].img_main_alt +
           '" class="img-fluid card-img-top">'
       );
       var $card_body = $('<div class="card-body">');
       var $title = $('<h4 class="card-title"></h4>');
-      $title.text(response[i].name);
+      $title.text($result[i].name);
       var $race = $('<small class="text-muted"></small>');
-      $race.text(response[i].race);
+      $race.text($result[i].race);
       var $hr = $("<hr>");
       var $sex = $('<p class="card-text"></p>');
-      $sex.text("Płeć: " + response[i].sex);
+      $sex.text("Płeć: " + $result[i].sex);
       var $age = $('<p class="card-text"></p>');
-      $age.text("wiek: " + response[i].age);
+      $age.text("wiek: " + $result[i].age);
       var $weight = $('<p class="card-text"></p>');
-      $weight.text("Wielkość: " + response[i].weight);
+      $weight.text("Wielkość: " + $result[i].weight);
       var $button = $('<a class="btn btn-outline-primary btn-block" href=#>');
-      $button.attr("href", "/animals/" + response[i].id);
+      $button.attr("href", "/animals/" + $result[i].id);
       $button.text("Więcej szczegółów");
 
       $animals_cards.append($card);
@@ -79,41 +79,62 @@ $(function() {
       $weight.insertAfter($age);
       $button.insertAfter($weight);
       $card_body.insertAfter($img);
+
+      // Pagination
+      var $previous = $('<li class="page-item"></li>');
+      var $a_previous = $(
+        '<a class="page-link" href="#"><span>&laquo;</span><span class="sr-only">Previous</span></a>'
+      );
+      // $a_previous.attr("href", response["previous"]);
+      $previous.append($a_previous);
+
+      var $next = $('<li class="page-item"></li>');
+      $a_next = $(
+        '<a class="page-link" href="#"><span>&raquo;</span><span class="sr-only">Next</span></a>'
+      );
+      // $a_next.attr("href", response["next"]);
+      $next.append($a_next);
+
+      $listpagination.append($previous);
+      $listpagination.append($next);
+
+      $a_previous.on("click", function(event) {
+        var $url = response["previous"];
+        $.ajax({
+          url: $url,
+          method: "GET"
+        }).done(function() {
+          location.reload();
+          alert("Książka usunięta");
+        });
+      });
     }
+  }
 
-    var $previous = $(
-      '<li class="page-item"><a class="page-link" href="#"><span>&laquo;</span><span class="sr-only">Previous</span></a></li>'
-    );
-    $previous.attr("href", response.previous);
+  $.ajax({
+    url: "http://127.0.0.1:8000/animals/",
+    method: "GET"
+  }).done(getAiimalList);
 
-    var $next = $(
-      '<li class="page-item"><a class="page-link" href="#"><span>&raquo;</span><span class="sr-only">Next</span></a></li>'
-    );
-    $next.attr("href", response.next);
-
-    $listpagination.append($previous);
-    $listpagination.append($next);
-
-    // <li class="page-item disabled">
-    //         <a class="page-link" href="#">
-    //             <span>&laquo;</span>
-    //             <span class="sr-only">Previous</span>
-    //         </a>
-    //     </li>
-    //     <li class="page-item active">
-    //         <a class="page-link" href="#">1</a>
-    //     </li>
-    //     <li class="page-item">
-    //         <a class="page-link" href="#">2</a>
-    //     </li>
-    //     <li class="page-item">
-    //         <a class="page-link" href="#">3</a>
-    //     </li>
-    // <li class="page-item">
-    //     <a class="page-link" href="#">
-    //         <span>&raquo;</span>
-    //         <span class="sr-only">Next</span>
-    //     </a>
-    // </li>
-  });
+  // <li class="page-item disabled">
+  //         <a class="page-link" href="#">
+  //             <span>&laquo;</span>
+  //             <span class="sr-only">Previous</span>
+  //         </a>
+  //     </li>
+  //     <li class="page-item active">
+  //         <a class="page-link" href="#">1</a>
+  //     </li>
+  //     <li class="page-item">
+  //         <a class="page-link" href="#">2</a>
+  //     </li>
+  //     <li class="page-item">
+  //         <a class="page-link" href="#">3</a>
+  //     </li>
+  // <li class="page-item">
+  //     <a class="page-link" href="#">
+  //         <span>&raquo;</span>
+  //         <span class="sr-only">Next</span>
+  //     </a>
+  // </li>
 });
