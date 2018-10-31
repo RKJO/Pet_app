788f94c4342e0f1e8680f96d633ade105c28a4e6
RKJO <rkjozwiak@gmail.com>
2018-10-31 08:24:58 +0100

    add .sh script for dayly update database by CRON

--- budrys_app_main/update_database.sh
+++ budrys_app_main/update_database.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+cd
+# run Vrtualenv
+source .virtualenvs/budrys_app_env/bin/activate
+
+cd domains/budrys.org/public_python/
+
+# Run Spiders
+
+scrapy crawl paluch_shelter
+scrapy crawl wroclaw_shelter
+scrapy crawl czestochowa_shelter
+scrapy crawl krakow_shelter
+
+# run Management script
+
+python manage.py delete_adopted
